36476c9841541d7d36f1f3a2effd51023ccf3f6f
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2020-09-23 12:10:55 +0200

    adding test file

--- test-data/setup.sh
+++ test-data/setup.sh
@@ -1,4 +1,6 @@
 
+# prerequisite: have keptn intalled with --use-case=continuous-delivery flag
+
 keptn create project litmus --shipyard=./shipyard.yaml
 
 keptn onboard service carts-db --chart=./carts-db/ --project=litmus
@@ -11,20 +13,21 @@ keptn send event new-artifact --project=litmus --service=carts --image=docker.io
 
 keptn add-resource --project=litmus --stage=chaos --service=carts --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
 
-keptn add-resource --project=litmus --stage=chaos --service=carts --resource=slo.yaml --resourceUri=slo.yaml
+keptn add-resource --project=litmus --stage=chaos --service=carts --resource=jmeter/jmeter.conf.yaml --resourceUri=jmeter/jmeter.conf.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/prometheus-service/release-0.3.5/deploy/service.yaml
-
-keptn configure monitoring prometheus --project=litmus --service=carts
 
 kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/prometheus-sli-service/release-0.2.2/deploy/service.yaml
 
 keptn add-resource --project=litmus --stage=chaos --service=carts --resource=prometheus/sli.yaml --resourceUri=prometheus/sli.yaml 
 
+keptn add-resource --project=litmus --stage=chaos --service=carts --resource=slo.yaml --resourceUri=slo.yaml
+
+keptn configure monitoring prometheus --project=litmus --service=carts
 
 ## now we also have to add the chaos tests - ATTENTION right now this file is empty!
 keptn add-resource --project=litmus --stage=chaos --service=carts --resource=litmus/experiment.yaml --resourceUri=litmus/experiment.yaml
 
-
+# now test with a a new-artifact event
 keptn send event new-artifact --project=litmus --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.1
 
