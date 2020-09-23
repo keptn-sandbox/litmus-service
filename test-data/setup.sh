6e24feb50845cb300e4b75f8f3afd3625efbbce5
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2020-09-23 14:34:20 +0200

    update setup instructions

--- test-data/setup.sh
+++ test-data/setup.sh
@@ -1,5 +1,10 @@
 
-# prerequisite: have keptn intalled with --use-case=continuous-delivery flag
+# prerequisite: have keptn installed with --use-case=continuous-delivery flag
+
+# 1. install Istio https://tutorials.keptn.sh/tutorials/keptn-full-tour-prometheus-07/index.html?index=..%2F..index#2 
+# 2. install Keptn https://tutorials.keptn.sh/tutorials/keptn-full-tour-prometheus-07/index.html?index=..%2F..index#4
+# 3. configure Istio + Keptn  shttps://tutorials.keptn.sh/tutorials/keptn-full-tour-prometheus-07/index.html?index=..%2F..index#5
+# 4. connect the Keptn CLI to the cluster https://tutorials.keptn.sh/tutorials/keptn-full-tour-prometheus-07/index.html?index=..%2F..index#6 
 
 keptn create project litmus --shipyard=./shipyard.yaml
 
@@ -25,9 +30,15 @@ keptn add-resource --project=litmus --stage=chaos --service=carts --resource=slo
 
 keptn configure monitoring prometheus --project=litmus --service=carts
 
+
+
+
 ## now we also have to add the chaos tests - ATTENTION right now this file is empty!
 keptn add-resource --project=litmus --stage=chaos --service=carts --resource=litmus/experiment.yaml --resourceUri=litmus/experiment.yaml
 
+## make sure that Litmus is installed in the cluster!
+
+
 # now test with a a new-artifact event
-keptn send event new-artifact --project=litmus --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.1
+keptn send event new-artifact --project=litmus --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.3
 
